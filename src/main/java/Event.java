d3dd51303a27e39be98a2d1bd70c907cc980fa72
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-27 18:07:52 +0200

    done

---
+++ src/main/java/Event.java
@@ -0,0 +1,21 @@
+public class Event{
+    private String name;
+    public Event(){}
+    public Event(String name)
+    {
+        this.name=name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
+}
