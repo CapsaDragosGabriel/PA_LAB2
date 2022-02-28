497d703868b2afb281e8e373d838d345502c1aba
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-02-28 16:05:55 +0200

    y

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class Homework {
-   public Room[] rooms = new Room[5];
-  public  Event[] events = new Event[7];
+   public Room[] rooms = new Room[10];
+  public  Event[] events = new Event[10];
     public int countEvents = 0;
     public int countRooms=0;
     public  int AddEvents(Event test) {
@@ -59,6 +61,16 @@ public class Homework {
         return countRooms;
     }
 
+    @Override
+    public String toString() {
+        return "Homework{" +
+                "\nrooms=" + Arrays.toString(rooms) +
+                "\nevents=" + Arrays.toString(events) +
+                "\ncountEvents=" + countEvents +
+                "\ncountRooms=" + countRooms +
+                '}';
+    }
+
     public static void main(String[] args) {
 
         Homework app = new Homework();
@@ -75,7 +87,7 @@ public class Homework {
         app.AddEvents(l2);
         app.AddEvents(l3);
         app.AddEvents(l3);
-        app.ShowEvents();
+
 
         Room r401=new Room("401",30,RoomType.COMPUTER_LAB);
         Room r403=new Room("403",30,RoomType.COMPUTER_LAB);
@@ -85,17 +97,9 @@ public class Homework {
         app.AddRooms(r403);
         app.AddRooms(r405);
         app.AddRooms(r309);
+
+       // System.out.println(app);
+        app.ShowEvents();
         app.ShowRooms();
-
-
     }
 }
- /*Problem p1 = new Problem();
-        Event c1 = new Event("C1", 100, 8, 10);
-        Event c2 = new Event("C2", 100, 10, 12);
-        Event l1 = new Event("L1", 30, 8, 10);
-        Event l2 = new Event("L2", 30, 8, 10);
-        Event l3 = new Event("L3", 30, 10, 12);
-        p1.AddEvents(c1);
-        p1.ShowEvents();
-*/
